a0b86ca129b1ad359a4cb1ebe6f83d3ce1e045f3
Gabriel Natan <gabrielnatan.2001.pereira@gmail.com>
2025-08-24 20:52:28 -0300

    ajsutando .env do background

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,4 +1,41 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+// Carrega variáveis do .env com caminho absoluto
+const envPath = path.resolve(__dirname, '.env');
+console.log('Tentando carregar .env de:', envPath);
+const envResult = dotenv.config({ path: envPath });
+
+if (envResult.error) {
+  console.error('Erro ao carregar .env:', envResult.error);
+}
+
+const envParsed = (envResult.parsed || {});
+console.log('Conteúdo do .env carregado:', envParsed);
+
+// Prepara objeto para o DefinePlugin apenas com VITE_*
+const viteEnvDefine = Object.keys(envParsed)
+  .filter((key) => key.startsWith('VITE_'))
+  .reduce((acc, key) => {
+    acc[`process.env.${key}`] = JSON.stringify(envParsed[key]);
+    return acc;
+  }, {});
+
+// Adiciona fallbacks para variáveis não definidas
+const fallbackEnvVars = {
+  'process.env.VITE_BASE_URL': JSON.stringify('http://localhost:3000/api'),
+  'process.env.VITE_ENVIRONMENT': JSON.stringify('development'),
+  'process.env.VITE_DEBUG': JSON.stringify('false'),
+  'process.env.VITE_LOG_LEVEL': JSON.stringify('info'),
+};
+
+// Combina as variáveis do .env com os fallbacks
+const finalEnvDefine = { ...fallbackEnvVars, ...viteEnvDefine };
+
+// Log para debug
+console.log('Variáveis de ambiente carregadas no main process:', Object.keys(finalEnvDefine));
+console.log('Valores finais das variáveis:', finalEnvDefine);
 
 module.exports = {
   /**
@@ -19,4 +56,11 @@ module.exports = {
   // Habilitar source maps para debug
   devtool: 'source-map',
   mode: 'development',
+  plugins: [
+    new webpack.DefinePlugin(finalEnvDefine),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.IS_ELECTRON': JSON.stringify(true)
+    })
+  ],
 };
